88e1b1edaf7f99c3488e66fb67f2cbc8691807ca
anindo <sarkeranindo@gmail.com>
2022-11-10 22:26:21 +0600

    subreddit postid

---
+++ pages/post/[postId].tsx
@@ -0,0 +1,91 @@
+import { useMutation, useQuery } from "@apollo/client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import React from "react";
+import Post from "../../components/Post";
+import { GET_POST_BY_POST_ID } from "../../graphql/queries";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { ADD_COMMENT } from "../../graphql/mutations";
+import { toast } from "react-hot-toast";
+
+type FormData = {
+  comment: string;
+};
+
+function PostPage() {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [addComment] = useMutation(ADD_COMMENT, {
+    refetchQueries: [GET_POST_BY_POST_ID, "getPost"],
+  });
+
+  const { data } = useQuery(GET_POST_BY_POST_ID, {
+    variables: {
+      id: router.query.postId,
+    },
+  });
+
+  const post: Post = data?.getPost;
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    //post comment here...
+    console.log(data);
+
+    const notification = toast.loading("Posting your comment...");
+
+    await addComment({
+      variables: {
+        post_id: router.query.postId,
+        username: session?.user?.name,
+        text: data.comment,
+      },
+    });
+
+    setValue('comment', '')
+
+    toast.success('Comment added :)', {
+      id: notification,
+    })
+  };
+
+  return (
+    <div className="mx-auto my-7 max-w-5xl">
+      <Post post={post} />
+
+      <div className="-mt-1 rounded-b-md border border-t-0 border-gray-300 bg-white p-5 pl-16">
+        <p className="text-sm">
+          Comment as <span className="text-red-500">{session?.user?.name}</span>
+        </p>
+
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2"
+        >
+          <textarea
+            {...register("comment")}
+            disabled={!session}
+            className="h-24 rounded-md border border-gray-200 p-2 pl-4 outline-none disabled:bg-gray-50"
+            placeholder={session ? "What are your thoughts" : "Sign in bitch"}
+          />
+
+          <button
+            type="submit"
+            className="rounded-full bg-red-500 p-3 font-semibold text-white disabled:bg-gray-200"
+          >
+            Comment
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default PostPage;
